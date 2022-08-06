426e6cd08b4201496dba0e00423693602221f6bc
fezud <fezudd@yandex.ru>
2022-08-06 10:21:57 +0300

    readme

--- truffle-config.js
+++ truffle-config.js
@@ -1,47 +1,47 @@
-require('dotenv').config()
+require("dotenv").config();
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 module.exports = {
-  contracts_build_directory: "./build",
+    contracts_build_directory: "./build",
 
-  networks: {
-    polygon: {
-      provider: () => {
-        return new HDWalletProvider({
-          privateKeys:  [process.env.PRIVATE_KEY],
-          providerOrUrl: "https://polygon-rpc.com",
-          chainId: 137,
-          pollingInterval: 30000
-        })
-      },
-      networkCheckTimeout: 10000,
-      gas: 7500000,
-      gasPrice: 70000000000,
-      network_id: 137,
-      addressIndex: 0
-    },
-    test: {
-      host: '127.0.0.1',
-      port: 8545,
-      gas: 7500000,
-      gasPrice: 297517,
-      network_id: 137
-    }
-  },
-  plugins: ["truffle-contract-size"],
-  compilers: {
-    solc: {
-      version: "0.8.10",
-      settings: {
-        optimizer: {
-          enabled: true,
-          runs: 200
+    networks: {
+        polygon: {
+            provider: () => {
+                return new HDWalletProvider({
+                    privateKeys: [process.env.PRIVATE_KEY],
+                    providerOrUrl: "https://polygon-rpc.com",
+                    chainId: 137,
+                    pollingInterval: 30000,
+                });
+            },
+            networkCheckTimeout: 10000,
+            gas: 7500000,
+            gasPrice: 70000000000,
+            network_id: 137,
+            addressIndex: 0,
         },
-      }
-    }
-  },
+        test: {
+            host: "127.0.0.1",
+            port: 8545,
+            gas: 7500000,
+            gasPrice: 1,
+            network_id: 137,
+        },
+    },
+    plugins: ["truffle-contract-size"],
+    compilers: {
+        solc: {
+            version: "0.8.10",
+            settings: {
+                optimizer: {
+                    enabled: true,
+                    runs: 200,
+                },
+            },
+        },
+    },
 
-  db: {
-    enabled: false
-  }
+    db: {
+        enabled: false,
+    },
 };
