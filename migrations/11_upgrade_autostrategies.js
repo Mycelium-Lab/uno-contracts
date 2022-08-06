f97137fa828e2e86f1c72b4697aa54ca9914f924
fezud <fezudd@yandex.ru>
2022-08-06 09:27:01 +0300

    tests

---
+++ migrations/11_upgrade_autostrategies.js
@@ -0,0 +1,10 @@
+const AutoStrategyFactory = artifacts.require('UnoAutoStrategyFactory')
+const AutoStrategy = artifacts.require('UnoAutoStrategy')
+
+module.exports = async function (deployer) {
+    await deployer.deploy(AutoStrategy) 
+    const autoStrategyFactory = await AutoStrategyFactory.deployed()
+    await autoStrategyFactory.upgradeStrategies(AutoStrategy.address)
+
+    console.log("Upgraded", AutoStrategyFactory.address)
+}
---
+++ migrations/6_upgrade_sushiswap.js
@@ -0,0 +1,28 @@
+const { upgradeProxy } = require('@openzeppelin/truffle-upgrades')
+const FarmFactory = artifacts.require('UnoFarmFactory')
+const { promises : fs } = require("fs")
+const path = require("path")
+
+const Farm = artifacts.require('UnoFarmSushiswap')
+const AssetRouter = artifacts.require('UnoAssetRouterSushiswap')
+
+module.exports = async function (deployer) {
+    //AssetRouter upgrade
+    const assetRouter = await AssetRouter.deployed()
+    const instance = await upgradeProxy(assetRouter.address, AssetRouter, { deployer })
+
+    //Farm upgrade
+    await deployer.deploy(Farm) 
+    const factoryAddress = await readFactoryAddress()
+    const farmFactory = await FarmFactory.at(factoryAddress)
+    await farmFactory.upgradeFarms(Farm.address)
+
+    console.log("Upgraded", instance.address)
+}
+
+async function readFactoryAddress(){
+    const data = await fs.readFile(path.resolve(__dirname, './addresses/factories.json'))
+    var json = JSON.parse(data)
+  
+    return json.sushiswap
+}
