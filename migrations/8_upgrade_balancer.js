ee1c0adf994a7a1f6b8a2f8f154d3613c53e0737
juglipaff <juglipaff@gmail.com>
2022-10-01 16:10:58 +0300

    deploy

--- migrations/8_upgrade_balancer.js
+++ migrations/8_upgrade_balancer.js
@@ -7,7 +7,7 @@ module.exports = async (deployer, network) => {
     if (network !== 'polygon') return
 
     // AssetRouter upgrade
-    const assetRouter = await AssetRouter.deployed()
+    const assetRouter = await AssetRouter.at('0xa9877C4cbd6b4c38604ee44a11948Aa4716D5b37')
     const impl = await prepareUpgrade(assetRouter.address, AssetRouter, { deployer })
 
     // Farm upgrade
