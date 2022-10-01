8203b8c07458de7f3d1ca5d81f436a5a2621ac2a
juglipaff <juglipaff@gmail.com>
2022-10-01 17:32:54 +0300

    deploys

--- migrations/2_upgrade_trisolaris_standard.js
+++ migrations/2_upgrade_trisolaris_standard.js
@@ -1,4 +1,4 @@
-const { prepareUpgrade } = require('@openzeppelin/truffle-upgrades')
+const { prepareUpgrade/* , forceImport */ } = require('@openzeppelin/truffle-upgrades')
 
 const Farm = artifacts.require('UnoFarmTrisolarisStandard')
 const AssetRouter = artifacts.require('UnoAssetRouterTrisolarisStandard')
@@ -6,9 +6,11 @@ const AssetRouter = artifacts.require('UnoAssetRouterTrisolarisStandard')
 module.exports = async (deployer, network) => {
     if (network !== 'aurora') return
 
+    // await forceImport('0xd19cfA4942E3aE4E94A53dEdee2A0a14F3FB4D97', AssetRouter)
+
     // AssetRouter upgrade
-    const assetRouter = await AssetRouter.deployed()
-    const impl = await prepareUpgrade(assetRouter.address, AssetRouter, { deployer })
+    // const assetRouter = await AssetRouter.at('0xd19cfA4942E3aE4E94A53dEdee2A0a14F3FB4D97')// '0xd19cfA4942E3aE4E94A53dEdee2A0a14F3FB4D97'
+    const impl = await prepareUpgrade('0xd19cfA4942E3aE4E94A53dEdee2A0a14F3FB4D97', AssetRouter, { deployer })
 
     // Farm upgrade
     await deployer.deploy(Farm)
