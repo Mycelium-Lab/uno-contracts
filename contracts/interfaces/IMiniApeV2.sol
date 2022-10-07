51cf634bf1db528f4959acc4c0ea4e81f6b7b8d4
fezud <fezudd@yandex.ru>
2022-10-07 15:03:38 +0300

    ApeSwap Integration

---
+++ contracts/interfaces/IMiniApeV2.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+pragma experimental ABIEncoderV2;
+
+interface IMiniApeV2 {
+
+    struct UserInfo {
+        uint256 amount;
+        int256 rewardDebt;
+    }
+
+    struct PoolInfo {
+        uint128 accBananaPerShare;
+        uint64 lastRewardTime;
+        uint64 allocPoint;
+    }
+
+    function BANANA() external view returns (address);
+    function poolInfo(uint256 pid) external view returns (PoolInfo memory);
+    function rewarder(uint256 pid) external view returns (address);
+    function lpToken(uint256 pid) external view returns (address);
+    function userInfo(uint256 arg1, address arg2) external view returns (UserInfo memory);
+    function totalAllocPoint() external view returns (uint256); 
+    function bananaPerSecond() external view returns (uint256);
+    function pendingBanana(uint256 _pid, address _user) external view returns (uint256 pending);
+    function poolLength() external view returns (uint256 pools);
+
+    function massUpdatePools(uint256[] calldata pids) external;
+    function updatePool(uint256 pid) external returns (PoolInfo memory pool);
+    function deposit(uint256 pid, uint256 amount, address to) external;
+    function withdraw(uint256 pid, uint256 amount, address to) external;
+    function harvest(uint256 pid, address to) external;
+    function withdrawAndHarvest(uint256 pid, uint256 amount, address to) external;
+    function emergencyWithdraw(uint256 pid, address to) external;
+}
---
+++ test/TestApeSwapFarm.js
@@ -0,0 +1,164 @@
+const {
+    expectEvent, expectRevert, BN, constants
+} = require('@openzeppelin/test-helpers')
+
+const IUniswapV2Pair = artifacts.require('IUniswapV2Pair')
+const IRewarder = artifacts.require('IMiniComplexRewarderTime')
+
+const Farm = artifacts.require('UnoFarmApeSwap')
+const IMiniApeV2 = artifacts.require('IMiniApeV2')
+
+const pool = '0x65D43B64E3B31965Cd5EA367D4c2b94c03084797' // usdt wmatic
+const miniApeAddress = '0x54aff400858Dcac39797a81894D9920f16972D1D'
+
+contract('UnoFarmApeSwap', (accounts) => {
+    const assetRouter = accounts[0]
+
+    let implementation
+    let stakingToken
+    let miniChef
+    let pid
+    let rewardToken
+    let rewarderToken
+
+    let receipt
+
+    before(async () => {
+        implementation = await Farm.new({ from: accounts[0] })
+
+        receipt = await implementation.initialize(pool, assetRouter, { from: accounts[0] })
+
+        miniChef = await IMiniApeV2.at(miniApeAddress)
+        const poolLength = await miniChef.poolLength()
+
+        for (let i = 0; i < poolLength.toNumber(); i++) {
+            const lpToken = await miniChef.lpToken(i)
+            if (lpToken.toString() === pool) {
+                pid = i
+                break
+            }
+        }
+
+        try {
+            const rewarderAddress = (await miniChef.rewarder(pid)).toString()
+
+            const rewarder = await IRewarder.at(rewarderAddress)
+            const data = await rewarder.pendingTokens(pid, constants.ZERO_ADDRESS, 0)
+            rewarderToken = data['0']['0'].toString()
+        } catch (error) {
+            rewarderToken = constants.ZERO_ADDRESS
+        }
+        stakingToken = await IUniswapV2Pair.at(pool)
+
+        rewardToken = await miniChef.BANANA()
+    })
+
+    describe('Emits initialize event', () => {
+        it('fires events', async () => {
+            expectEvent(receipt, 'Initialized', { version: new BN(1) })
+        })
+    })
+
+    describe("Can't call multiple initializations", () => {
+        it('Reverts', async () => {
+            await expectRevert(
+                implementation.initialize(pool, assetRouter, { from: accounts[0] }),
+                'Initializable: contract is already initialized'
+            )
+        })
+    })
+
+    describe('Initializes variables', () => {
+        it('Sets pool ID', async () => {
+            assert.equal(
+                await implementation.pid(),
+                pid,
+                'PID is not correct'
+            )
+        })
+        it('Sets lpPair', async () => {
+            assert.equal(
+                await implementation.lpPair(),
+                stakingToken.address,
+                'Staking token is not correct'
+            )
+        })
+        it('Sets rewardToken', async () => {
+            assert.equal(
+                await implementation.rewardToken(),
+                rewardToken,
+                'Reward token is not correct'
+            )
+        })
+        it('Sets rewarderToken', async () => {
+            assert.equal(
+                await implementation.rewarderToken(),
+                rewarderToken,
+                'Reward token is not correct'
+            )
+        })
+        it('Sets tokens', async () => {
+            assert.equal(
+                await implementation.tokenA(),
+                await stakingToken.token0(),
+                'TokenA is not correct'
+            )
+            assert.equal(
+                await implementation.tokenB(),
+                await stakingToken.token1(),
+                'TokenB is not correct'
+            )
+        })
+        it('Sets assetRouter', async () => {
+            assert.equal(
+                await implementation.assetRouter(),
+                assetRouter,
+                'assetRouter is not correct'
+            )
+        })
+    })
+
+    describe('functions available only for asset router', () => {
+        // CALLER_NOT_ASSET_ROUTER check fails
+        it('Prevents function calls for not asset router', async () => {
+            await expectRevert(
+                implementation.deposit(0, 0, 0, 0, 0, accounts[0], accounts[0], { from: accounts[1] }),
+                'CALLER_NOT_ASSET_ROUTER'
+            )
+            await expectRevert(
+                implementation.withdraw(0, 0, 0, false, accounts[0], accounts[0], { from: accounts[1] }),
+                'CALLER_NOT_ASSET_ROUTER'
+            )
+            await expectRevert(
+                implementation.distribute(
+                    [{ route: [], amountOutMin: 0 }, { route: [], amountOutMin: 0 }, { route: [], amountOutMin: 0 }, { route: [], amountOutMin: 0 }],
+                    [{ route: [], amountOutMin: 0 }, { route: [], amountOutMin: 0 }],
+                    { feeTo: accounts[1], fee: 0 },
+                    { from: accounts[1] }
+                ),
+                'CALLER_NOT_ASSET_ROUTER'
+            )
+        })
+
+        // ASSET_ROUTER check passes, revert for a different reason
+        it('Allows function calls for asset router', async () => {
+            await expectRevert(
+                implementation.deposit(0, 0, 0, 0, 0, accounts[0], accounts[0], { from: assetRouter }),
+                'NO_LIQUIDITY_PROVIDED'
+            )
+            await expectRevert(
+                implementation.withdraw(0, 0, 0, false, accounts[0], accounts[0], { from: assetRouter }),
+                'INSUFFICIENT_AMOUNT'
+            )
+            await expectRevert(
+                implementation.distribute(
+                    [{ route: [], amountOutMin: 0 }, { route: [], amountOutMin: 0 }, { route: [], amountOutMin: 0 }, { route: [], amountOutMin: 0 }],
+                    [{ route: [], amountOutMin: 0 }, { route: [], amountOutMin: 0 }],
+                    { feeTo: accounts[1], fee: 0 },
+                    { from: assetRouter }
+                ),
+                'NO_LIQUIDITY'
+            )
+        })
+    })
+})
