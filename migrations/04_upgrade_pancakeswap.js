fd3f0feee291f48a586eb218b838ae69ab013709
juglipaff <juglipaff@gmail.com>
2022-12-14 14:28:55 +0300

    add defender proposal upgrade

--- migrations/04_upgrade_pancakeswap.js
+++ migrations/04_upgrade_pancakeswap.js
@@ -1,19 +1,100 @@
-const { prepareUpgrade } = require('@openzeppelin/truffle-upgrades')
-
 const Farm = artifacts.require('UnoFarmPancakeswap')
 const AssetRouter = artifacts.require('UnoAssetRouterPancakeswap')
 
+require('dotenv').config()
+const ethers = require('ethers')
+const { prepareUpgrade } = require('@openzeppelin/truffle-upgrades')
+const fs = require('fs/promises')
+const path = require('path')
+const { AdminClient } = require('defender-admin-client')
+
+const client = new AdminClient({ apiKey: process.env.DEFENDER_ACCOUNT, apiSecret: process.env.DEFENDER_PASSWORD })
+
+async function readAddress(app) {
+    const data = await fs.readFile(path.resolve(__dirname, './addresses/addresses.json'))
+    const json = JSON.parse(data)
+
+    return json[app]
+}
+
 module.exports = async (deployer, network) => {
-    if (network !== 'bsc') return
+    if (network !== 'polygon') return
 
-    // AssetRouter upgrade
-    const assetRouter = await AssetRouter.at('0x67C8aFF20754629308Ce5bE49F5cfEEF5c7D5296')
-    const impl = await prepareUpgrade(assetRouter.address, AssetRouter, { deployer })
+    const multisig = await readAddress('multisig')
+    const timelockAddress = await readAddress('timelock')
+    {
+        // AssetRouter upgrade
+        const UnoAssetRouter = await readAddress('pancakeswap-router')
+        const impl = await prepareUpgrade(UnoAssetRouter, AssetRouter, { deployer })
+        console.log('New Router implementation:', impl) // upgradeTo(newImplementation)
 
-    // Farm upgrade
-    await deployer.deploy(Farm)
+        const ABI = ['function upgradeTo(address newImplementation)']
+        const data = (new ethers.utils.Interface(ABI)).encodeFunctionData('upgradeTo', [impl])
+        const timelock = {
+            target: UnoAssetRouter,
+            value: '0',
+            data,
+            salt: ethers.BigNumber.from(ethers.utils.randomBytes(32))._hex,
+            address: timelockAddress,
+            delay: '172800'
+        }
+        timelock.operationId = ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(
+            ['address', 'uint256', 'bytes', 'bytes32', 'bytes32'],
+            [timelock.target, timelock.value, timelock.data, '0x0000000000000000000000000000000000000000000000000000000000000000', timelock.salt]
+        ))
 
-    console.log('New Router implementation:', impl) // UpgradeTo(newImplementation)
-    console.log('New Farm implementation:', Farm.address) // UpgradeFarms(newImplementation)
-    // Create proposals using Openzeppelin Defender UI
+        const proposal = await client.createProposal({
+            contractId: `bsc-${UnoAssetRouter}`, // Target contract
+            title: 'Upgrade', // Title of the proposal
+            description: 'Upgrade', // Description of the proposal
+            type: 'custom', // Use 'custom' for custom admin actions
+            targetFunction: { name: 'upgradeTo', inputs: [{ type: 'address', name: 'newImplementation' }] }, // Function ABI
+            functionInputs: [impl], // Arguments to the function
+            via: multisig, // Address to execute proposal
+            viaType: 'Gnosis Safe', // 'Gnosis Safe', 'Gnosis Multisig', or 'EOA'
+            timelock,
+            metadata: { sendValue: '0' },
+            isArchived: false
+        })
+        console.log('Proposal:', proposal.url)
+    }
+
+    {
+        // Farm upgrade
+        const UnoFarmFactory = await readAddress('pancakeswap-factory')
+
+        await deployer.deploy(Farm)
+        const impl = Farm.address
+        console.log('New Farm implementation:', impl) // upgradeFarms(newImplementation)
+
+        const ABI = ['function upgradeFarms(address newImplementation)']
+        const data = (new ethers.utils.Interface(ABI)).encodeFunctionData('upgradeFarms', [impl])
+        const timelock = {
+            target: UnoFarmFactory,
+            value: '0',
+            data,
+            salt: ethers.BigNumber.from(ethers.utils.randomBytes(32))._hex,
+            address: timelockAddress,
+            delay: '172800'
+        }
+        timelock.operationId = ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(
+            ['address', 'uint256', 'bytes', 'bytes32', 'bytes32'],
+            [timelock.target, timelock.value, timelock.data, '0x0000000000000000000000000000000000000000000000000000000000000000', timelock.salt]
+        ))
+
+        const proposal = await client.createProposal({
+            contractId: `bsc-${UnoFarmFactory}`, // Target contract
+            title: 'Upgrade', // Title of the proposal
+            description: 'Upgrade', // Description of the proposal
+            type: 'custom', // Use 'custom' for custom admin actions
+            targetFunction: { name: 'upgradeFarms', inputs: [{ type: 'address', name: 'newImplementation' }] }, // Function ABI
+            functionInputs: [impl], // Arguments to the function
+            via: multisig, // Address to execute proposal
+            viaType: 'Gnosis Safe', // 'Gnosis Safe', 'Gnosis Multisig', or 'EOA'
+            timelock,
+            metadata: { sendValue: '0' },
+            isArchived: false
+        })
+        console.log('Proposal:', proposal.url)
+    }
 }
