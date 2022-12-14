9039e78dae2979cda5116d5d983fef37a08fbca1
juglipaff <juglipaff@gmail.com>
2022-12-14 18:08:26 +0300

    fix upgrade script

--- migrations/04_upgrade_pancakeswap.js
+++ migrations/04_upgrade_pancakeswap.js
@@ -18,7 +18,7 @@ async function readAddress(app) {
 }
 
 module.exports = async (deployer, network) => {
-    if (network !== 'polygon') return
+    if (network !== 'bsc') return
 
     const multisig = await readAddress('multisig')
     const timelockAddress = await readAddress('timelock')
