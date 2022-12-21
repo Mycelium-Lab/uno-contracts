050ba5d1b3fd44f7132effa16338ebf54ec21436
juglipaff <juglipaff@gmail.com>
2022-12-21 21:31:28 +0300

    add defender upgrade

--- migrations/1_deploy_traderjoe.js
+++ migrations/1_deploy_traderjoe.js
@@ -5,22 +5,18 @@ const FarmFactory = artifacts.require('UnoFarmFactory')
 const { promises: fs } = require('fs')
 const path = require('path')
 
-const { distributor, pauser } = require('./addresses/addresses')
-
 const Farm = artifacts.require('UnoFarmTraderjoe')
 const AssetRouter = artifacts.require('UnoAssetRouterTraderjoe')
 
-async function addFactoryAddress(address) {
-    const data = await fs.readFile(path.resolve(__dirname, './addresses/factories.json'))
+async function addAddress(key, address) {
+    const data = await fs.readFile(path.resolve(__dirname, './addresses/addresses.json'))
     const json = JSON.parse(data)
 
-    json.traderjoe = address
-    await fs.writeFile(path.resolve(__dirname, './addresses/factories.json'), JSON.stringify(json))
+    json[key] = address
+    await fs.writeFile(path.resolve(__dirname, './addresses/addresses.json'), JSON.stringify(json))
 }
 
 module.exports = async (deployer, network, accounts) => {
-    console.log(network)
-
     if (network !== 'avalanche') return
     // AccessManager deployment, dont deploy if already deployed on this network
     await deployer.deploy(AccessManager, { overwrite: false, from: accounts[0] })
@@ -33,23 +29,8 @@ module.exports = async (deployer, network, accounts) => {
     )
     // Deploy Factory
     await deployer.deploy(FarmFactory, Farm.address, AccessManager.address, assetRouter.address)
-    await addFactoryAddress(FarmFactory.address)
 
-    await assetRouter.setFee('40000000000000000')
-
-    const accessManager = await AccessManager.deployed()
-    // DISTRIBUTOR_ROLE
-    const distributorHasRole = await accessManager.hasRole('0xfbd454f36a7e1a388bd6fc3ab10d434aa4578f811acbbcf33afb1c697486313c', distributor)
-    if (!distributorHasRole) {
-        await accessManager.grantRole('0xfbd454f36a7e1a388bd6fc3ab10d434aa4578f811acbbcf33afb1c697486313c', distributor)
-        console.log('Distributor set')
-    }
-    // PAUSER_ROLE
-    const pauserHasRole = await accessManager.hasRole('0x65d7a28e3265b37a6474929f336521b332c1681b933f6cb9f3376673440d862a', pauser)
-    if (!pauserHasRole) {
-        await accessManager.grantRole('0x65d7a28e3265b37a6474929f336521b332c1681b933f6cb9f3376673440d862a', pauser)
-        console.log('Pauser set')
-    }
-
-    console.log('Deployed', assetRouter.address)
+    await addAddress('access-manager', AccessManager.address)
+    await addAddress('traderjoe-router', assetRouter.address)
+    await addAddress('traderjoe-factory', FarmFactory.address)
 }
